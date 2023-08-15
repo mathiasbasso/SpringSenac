48552a97840f669b074b5fba4cd126cc2386c04e
mathiasbasso <mathias201711@gmail.com>
2023-08-15 19:53:31 -0300

    Chegaando no git

--- SenacGames/src/main/java/com/senac/SenacGames/SenacGamesApplication.java
+++ SenacGames/src/main/java/com/senac/SenacGames/SenacGamesApplication.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class SenacGamesApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(SenacGamesApplication.class, args);
 	}
