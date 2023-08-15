a3b7eb423a0e5e7444c9a643e4c91cf7971b673b
mathiasbasso <mathias201711@gmail.com>
2023-08-15 20:19:04 -0300

    Chegaando no git

---
+++ SenacGames/src/main/java/com/senac/SenacGames/controller/GameController.java
@@ -0,0 +1,23 @@
+package com.senac.SenacGames.controller;
+
+import com.senac.SenacGames.entities.Game;
+import com.senac.SenacGames.repository.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/games")
+public class GameController {
+   @Autowired
+    private GameRepository gameRepository;
+
+   @GetMapping
+    public List<Game> getTodosOsJogos(){
+       return gameRepository.findAll();
+   }
+
+}
---
+++ SenacGames/src/main/java/com/senac/SenacGames/entities/Game.java
@@ -0,0 +1,31 @@
+package com.senac.SenacGames.entities;
+
+public class Game {
+    private Long id;
+    private String nome;
+    private String genero;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
