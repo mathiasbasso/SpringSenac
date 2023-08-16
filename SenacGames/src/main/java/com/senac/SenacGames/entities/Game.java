03440a972245f6e63549af801cc70c5f536037e8
mathiasbasso <mathias201711@gmail.com>
2023-08-15 22:00:51 -0300

    opa

--- SenacGames/src/main/java/com/senac/SenacGames/entities/Game.java
+++ SenacGames/src/main/java/com/senac/SenacGames/entities/Game.java
@@ -1,6 +1,14 @@
 package com.senac.SenacGames.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String genero;
