bcb0f67d9f42aa3a184692e468b89cc491c0e5cc
mathiasbasso <mathias201711@gmail.com>
2023-09-13 21:29:27 -0300

    ...

--- Jogo-da-Velha-Java-Spring-master/src/main/resources/static/jogo.js
+++ Jogo-da-Velha-Java-Spring-master/src/main/resources/static/jogo.js
@@ -5,6 +5,8 @@
 
 function myfunc() {
 
+	var x = document.querySelector('#nameX');
+	x.value = nameX.text;
     
 	var b1, b2, b3, b4, b5, b6, b7, b8, b9;
 	b1 = document.getElementById("b1").value;
