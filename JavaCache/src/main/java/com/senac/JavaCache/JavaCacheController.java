33964db640416530644810d9dfe3be8299198a57
mathiasbasso <mathias201711@gmail.com>
2023-09-18 20:29:25 -0300

    commit

--- JavaCache/src/main/java/com/senac/JavaCache/JavaCacheController.java
+++ JavaCache/src/main/java/com/senac/JavaCache/JavaCacheController.java
@@ -1,5 +1,13 @@
 package com.senac.JavaCache;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class JavaCacheController {
+    private final JavaCacheService javaCacheService ;
+    public JavaCacheController() {
+        javaCacheService = null;
+    }
     
 }
+
