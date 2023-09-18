e32bf12cb031ac80ee52313421b09fd1f2d9c39b
mathiasbasso <mathias201711@gmail.com>
2023-09-18 20:23:51 -0300

    ...

---
+++ JavaCache/src/main/java/com/senac/JavaCache/JavaCacheController.java
@@ -0,0 +1,5 @@
+package com.senac.JavaCache;
+
+public class JavaCacheController {
+    
+}
--- JavaCache/src/main/java/com/senac/JavaCache/JavaCacheService.java
+++ JavaCache/src/main/java/com/senac/JavaCache/JavaCacheService.java
@@ -1,7 +1,17 @@
 package com.senac.JavaCache;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JavaCacheService {
+    @Cacheable(value = "teste", key = "#key")
+    public String getValor(String valor){
+     return "Retorno é: "+ valor;
+    }
+    @CacheEvict(value = "teste", allEntries = true)
+    public void limparCache(){
+
+    }
 }
