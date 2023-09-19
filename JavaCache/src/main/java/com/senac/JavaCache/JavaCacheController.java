6ba739f77d1cff42dcf9cc22a564acfe56417670
mathiasbasso <mathias201711@gmail.com>
2023-09-18 21:26:22 -0300

    commiiit

--- JavaCache/src/main/java/com/senac/JavaCache/JavaCacheController.java
+++ JavaCache/src/main/java/com/senac/JavaCache/JavaCacheController.java
@@ -1,13 +1,27 @@
 package com.senac.JavaCache;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 public class JavaCacheController {
-    private final JavaCacheService javaCacheService ;
-    public JavaCacheController() {
-        javaCacheService = null;
+    private final JavaCacheService javaCacheService;
+
+    @Autowired
+    public JavaCacheController(JavaCacheService javaCacheService){
+        this.javaCacheService = javaCacheService;
     }
-    
+    @GetMapping("/getCacheValue")
+    public String getCacheValue(@RequestParam String key){
+        return javaCacheService.getValor(key);
+    }
+    @PostMapping("/cleanCache")
+    public void cleanCache(){
+        javaCacheService.limparCache();
+    }
+
 }
 
